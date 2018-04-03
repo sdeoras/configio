b8ec2a02c104276c5c356f2db71306c95216db5b
Saurabh Deoras <sdeoras@gmail.com>
2018-04-03 10:52:01 -0700

    added watch

--- configfile/manager_test.go
+++ configfile/manager_test.go
@@ -2,8 +2,6 @@ package configfile
 
 import (
 	"context"
-	"encoding/json"
-	"io/ioutil"
 	"reflect"
 	"testing"
 	"time"
@@ -17,6 +15,7 @@ func TestNewWriter(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer writer.Close()
 
 	if err := writer.Marshal(new(simpleconfig.Config).Rand()); err != nil {
 		t.Fatal(err)
@@ -25,17 +24,21 @@ func TestNewWriter(t *testing.T) {
 
 func TestNewReader(t *testing.T) {
 	config := new(simpleconfig.Config).Rand()
-	b, err := json.MarshalIndent(config, "", "  ")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := ioutil.WriteFile(DefaultConfigFile, b, 0666); err != nil {
-		t.Fatal(err)
-	}
-
 	config2 := new(simpleconfig.Config)
+
 	reader, err := NewReader(context.Background())
 	if err != nil {
+		t.Fatal(err)
+	}
+	defer reader.Close()
+
+	writer, err := NewWriter(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer writer.Close()
+
+	if err := writer.Marshal(config); err != nil {
 		t.Fatal(err)
 	}
 
@@ -50,17 +53,18 @@ func TestNewReader(t *testing.T) {
 
 func TestNewReadWriter(t *testing.T) {
 	config := new(simpleconfig.Config).Rand()
+	config2 := new(simpleconfig.Config)
 
 	readWriter, err := NewReadWriter(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer readWriter.Close()
 
 	if err := readWriter.Marshal(config); err != nil {
 		t.Fatal(err)
 	}
 
-	config2 := new(simpleconfig.Config)
 	if err := readWriter.Unmarshal(config2); err != nil {
 		t.Fatal(err)
 	}
